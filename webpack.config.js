c8088df5a5a1232c557496af5c824057940f2f12
Bryan Liang <bryanliang1995@gmail.com>
2019-08-14 20:25:00 -0700

    Have webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$|\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
